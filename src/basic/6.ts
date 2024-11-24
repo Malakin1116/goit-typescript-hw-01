955cc0a589af6cf96364917ec6270a1df0ce4e81
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-11-24 17:47:14 +0200

    6

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -1,20 +1,28 @@
 // Створіть інтерфейс User для типізації об'єктів, які містять такі властивості.
 //  Зверніть увагу, що адреса є необов'язковою властивістю.
 
-// const mango = {
-//     name: 'Mango',
-//     age: 30,
-//     email: 'john@example.com',
-//     address: {
-//         city: 'New York',
-//         country: 'USA'
-//     }
-// };
+interface Config {
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city: string;
+    country: string;
+  };
+}
 
-// const poly = {
-//     name: 'Mango',
-//     age: 30,
-//     email: 'john@example.com'
-// };
+const mango: Config = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
 
-string || number || any;
+const poly: Config = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
